c965c421a1deed9fc09efa554effcba8a0cdb020
mbhrd <adrian.parham@yahoo.com>
2017-05-07 23:56:15 +0430

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using BL;
+using DA;
+
+
+namespace _3layer_university
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            student st = new student();
+            dataGridView1.DataSource = st.select();          
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            frmadd fa = new frmadd();
+            fa.ShowDialog();
+      
+            Form1_Load(null, null);
+        }
+
+       
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmlogin fl = new frmlogin();
+            fl.ShowDialog();
+        
+      
+        }
+    }
+}
---
+++ student.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using BL;
+using DA;
+
+
+namespace BL
+{
+    public class lesson : dataaccesslayer
+    {
+        public int lessonid;
+        public string fieldid;
+        public int lessonname;
+        public string teachername;
+        public string classhour;
+        public DataTable selectlesson()
+        {
+            base.connect();
+            string sql = "select * from lesson";
+            sql = string.Format(sql);
+            DataTable dt = base.select(sql);
+            base.disconnect();
+            return dt;
+        }
+    }
+   public  class student:dataaccesslayer 
+    {
+       public string name;
+       public string family;
+       public int shid;
+       public string address;
+       public string tell;
+       public string gender;
+       public int fieldcode;
+       public void add()
+       {
+           base.connect();
+           string sql = "insert into student (name,family,shid,address,tell,gender,fieldcode) values ( '{0}' , '{1}' , {2} , '{3}' , '{4}' , '{5}' , {6} )";
+           sql = string.Format(sql, name, family, shid, address, tell, gender, fieldcode);
+           base.docommand(sql);
+           base.disconnect();
+       }
+   
+
+    
+       public DataTable select()
+       {
+           base.connect();
+           string sql = "select * from student";
+           DataTable dt = base.select(sql);
+           base.disconnect();
+           return dt;
+       }
+       public DataTable selectone()
+       {
+           base.connect();
+           string sql = "select * from student where shid={0}";
+           sql = string.Format(sql, shid);
+           DataTable dt = base.select(sql);
+           base.disconnect();
+           return dt;
+       }
+      
+
+    }
+}
